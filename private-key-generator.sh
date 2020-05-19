edd8201bb26a55190e75a883bdf2d3ee7baf158e
root <darryl@leisue.com>
2020-05-19 07:20:56 +0000

    fixed bugs

--- private-key-generator.sh
+++ private-key-generator.sh
@@ -1,6 +1,2 @@
-ssh-keygen -t rsa -b 2048 -f private.key
-# Don't add passphrase
-openssl rsa -in private.key -pubout -outform PEM -out public.key
-cat private.key
-cat public.key
-
+openssl rand -base64 756 > keyfile
+chmod 400 keyfile
--- replica.sh
+++ replica.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-
-mongod --config "/data/configs/rs1.conf" --fork &
-sleep 1s 
-mongod --config "/data/configs/rs2.conf" --fork &
+DIR=/data/mongo-replica-config
+mongod --config "${DIR}/configs/rs1.conf" --fork &
+#sleep 1s 
+#mongod --config "${DIR}/configs/rs2.conf" --fork &
 #mongod --replSet kyoodb --logpath "/logs/1.log" --dbpath /data/kyoo_db/rs1 --port 27017 &
 #mongod --replSet kyoodb --logpath "/logs/2.log" --dbpath /data/kyoo_db/rs2 --port 27018 &
